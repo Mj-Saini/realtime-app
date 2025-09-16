ab5b72459d21f3c6246fbb296adefc33d28819ee
mj-saini <manojsaini97285@gmail.com>
2025-09-16 10:46:52 +0530

    change ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,6 @@
 // }
 
 
-
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
@@ -68,7 +67,7 @@ export default function App() {
     };
 
     return (
-        <div className="flex flex-col h-screen bg-gray-900 text-gray-200">
+        <div className="flex flex-col h-[100dvh] bg-gray-900 text-gray-200">
             {/* Chat Messages */}
             <div className="flex-1 overflow-y-auto p-4 space-y-3">
                 {messages.map((msg, i) => (
